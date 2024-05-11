15799127ac0a6a6b35fa46d55e10ee8fba7056c3
sanyancha <31632907+sanyancha@users.noreply.github.com>
2024-05-11 10:13:16 +0300

    add method

---
+++ own Pyramid Generator/21_methods.js
@@ -0,0 +1,12 @@
+// A method in JavaScript is a function that's associated with certain values or objects. An example you've already encountered is the .log() method, which is part of the console object.
+
+// Arrays have their own methods, and the first you will explore is the .push() method. This allows you to "push" a value to the end of an array. Here is an example to add the number 12 to the end of an array:
+
+// array.push(12);
+// Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
