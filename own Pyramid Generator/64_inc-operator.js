30cf0a3ffdeb2e7998064ef24eb73a4c428d28d8
sanyancha <31632907+sanyancha@users.noreply.github.com>
2024-05-12 14:42:41 +0300

    add increment operator

---
+++ own Pyramid Generator/64_inc-operator.js
@@ -0,0 +1,27 @@
+// Your pyramid generator now functions as expected. But this is an excellent opportunity to further explore the code you have written.
+
+// The addition operator is not the only way to add values to a variable. The addition assignment operator can be used as shorthand to mean "take the original value of the variable, add this value, and assign the result back to the variable." For example, these two statements would yield the same result:
+
+// test = test + 1;
+// test += 1;
+// Update your iterator statement in the for loop to use addition assignment.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i += 1) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
---
+++ own Pyramid Generator/65-increment-for.js
@@ -0,0 +1,25 @@
+// Because you are only increasing i by 1, you can use the increment operator ++. This operator increases the value of a variable by 1, updating the assignment for that variable. For example, test would become 8 here:
+
+// let test = 7;
+// test++;
+// Replace your addition assignment with the increment operator for your loop iteration.
+
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
+}
+
+for (let i = 0; i < count; i ++) {
+  rows.push(padRow(i + 1, count));
+}
+
+let result = ""
+
+for (const row of rows) {
+  result = result + "\n" + row;
+}
+
+console.log(result);
