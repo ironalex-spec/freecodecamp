4a9b5b7bcb23973150b94231f011a0dd6bf57a19
sanyancha <31632907+sanyancha@users.noreply.github.com>
2024-05-13 11:46:03 +0300

    add query 3 buttons

---
+++ Role playing game/17-make-const-var.js
@@ -0,0 +1,13 @@
+// button1 is a variable that is not going to be reassigned. If you are not going to assign a new value to a variable, it is best practice to use the const keyword to declare it instead of the let keyword. This will tell JavaScript to throw an error if you accidentally reassign it.
+
+// Change your button1 variable to be declared with the const keyword.
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
---
+++ Role playing game/18-add-query-3-buttons.js
@@ -0,0 +1,13 @@
+// Use querySelector() to get the other two button elements using their ids: button2 and button3. Store them in variables called button2 and button3. Remember to use const.
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+let inventory = ["stick"];
+
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
