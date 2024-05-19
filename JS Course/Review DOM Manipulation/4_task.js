4f66ea702d876414663688275c40cd2428c0e559
sanyancha <31632907+sanyancha@users.noreply.github.com>
2024-05-19 13:50:05 +0300

    4 task update

--- JS Course/Review DOM Manipulation/4_task.js
+++ JS Course/Review DOM Manipulation/4_task.js
@@ -1,13 +1,13 @@
-<!-- Now it is time to update the scores and the round results message.
+// <!-- Now it is time to update the scores and the round results message.
 
-Complete the showResults function. The playerScoreSpanElement and computerScoreSpanElement should be updated to show the updated scores of the player and computer.
+// Complete the showResults function. The playerScoreSpanElement and computerScoreSpanElement should be updated to show the updated scores of the player and computer.
 
-The roundResultsMsg should also be updated with the result of the round.
+// The roundResultsMsg should also be updated with the result of the round.
 
-Tips
+// Tips
 
-Remember that you learned how to work with the innerText property to update the text content of an element.
-You can use the getRoundResults function to get the result of the round. -->
+// Remember that you learned how to work with the innerText property to update the text content of an element.
+// You can use the getRoundResults function to get the result of the round. -->
 
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
